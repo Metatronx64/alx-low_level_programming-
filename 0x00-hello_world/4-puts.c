94fe6c5d072f696f523d3ee8965c65d8a5d4177a
Osagie Ofunmwegbe-Evba <osagieevba@gmail.com>
2022-12-08 15:00:17 -0800

    first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>                                                                                                                      
+/**                                                                                                                                     
+*                                                                                                                          
+* Return:  0                                                                                                                     
+*/                                                                                                                                     
+int main(void)                                                                                                                          
+{                                                                                                                                       
+        puts("\"Programming is like building a multi lingual puzzle);                                                            
+        return (0);
+}
