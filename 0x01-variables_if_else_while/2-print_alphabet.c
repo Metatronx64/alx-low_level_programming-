10ccfec90516ef09486e9bb2dcfd1d58d27651b6
root <osagieevba@gmail.com>
2022-12-17 14:27:51 -0800

    char lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,33 +1,24 @@
+
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Description: print all aplhabet letters
  *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
-
 {
+	char ch = 'a';
 
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		++ch;
+	}
+	putchar('\n');
 
-
-		char c;
-
-
-
-			for (c = 'a'; c <= 'z'; c++)
-
-						putchar(c);
-
-					putchar('\n');
-
-
-
-					return (0);
-
+	return (0);
 }
