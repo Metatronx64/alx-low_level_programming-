0ed89285184ea523dfc8664b8e06f83df34335ac
Metatronx64 <osagieevba@gmail.com>
2023-02-23 11:43:08 -0800

    fixed betty

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,10 @@
-#include "main.h"
-
 /**
- *  * _isdigit - checks if input is digit between 0 - 9
- *   *
- *    * @c: input
- *     *
- *      * Return: 1 if is digit, 0 if not
- *      */
+ * _isdigit - checks if input is digit between 0 - 9
+ *
+ * @c: input
+ *
+ * Return: 1 if is digit, 0 if not
+*/
 
 int _isdigit(int c)
 {
