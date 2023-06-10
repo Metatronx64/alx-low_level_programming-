f3bb4d046b7ab2224706d4422fc20856483f8deb
Metatronx64 <osagieevba@gmail.com>
2023-06-09 21:56:16 -0700

    hash tables

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Retrieves the index at which a key/value pair
+ *             should be stored in the array of a hash table.
+ * @key: The key to retrieve the index of.
+ * @size: The size of the array of the hash table.
+ *
+ * Return: The index of the key.
+ *
+ * Description: This function uses the djb2 algorithm
+ *              to calculate the index of the key.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
+
