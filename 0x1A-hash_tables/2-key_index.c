5de3fdd07675231b69a241477a2fbe5dee621fba
Metatronx64 <osagieevba@gmail.com>
2023-06-14 20:33:47 -0700

    add key index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -12,7 +12,8 @@
  *              to calculate the index of the key.
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
-{
+{	if (!size)
+		return (0);
 	return (hash_djb2(key) % size);
 }
 
